3404422d2d60cab29fd9943d7a8d3be741d4b1ab
garystarr-surgi <gary.starr@surgishop.com>
2025-11-30 14:16:40 -0500

    Update customer_statement_client.js

--- customer_statement/public/js/customer_statement_client.js
+++ customer_statement/public/js/customer_statement_client.js
@@ -1,25 +1,24 @@
 frappe.ui.form.on('Customer', {
     refresh: function(frm) {
-        // Only show the button if the document is saved and not new
-        if (frm.doc.docstatus === 0 && !frm.is_new()) {
-            
-            // Add the 'Statement' button next to the primary action buttons
+        // Only show the button if the document is saved (not new)
+        if (!frm.is_new()) {
+
+            // Add the 'Statement' button under the 'View Report' group
             frm.add_custom_button(__('Statement'), function() {
-                
+
                 // Get the current customer's name
                 const customer_name = frm.doc.name;
-                
+
                 // Open the custom report with filters pre-filled
-                // NOTE: 'Customer Statement' must exactly match the name of your report in the JSON file
+                // NOTE: 'Customer Statement' must exactly match the report name in your JSON file
                 frappe.set_route('query-report', 'Customer Statement', {
-                    // Pass the customer name to the report's 'customer' filter
                     customer: customer_name,
-                    // Pass current date and a default start date (e.g., 1 year ago)
-                    start_date: frappe.datetime.add_months(frappe.datetime.get_today(), -12),
+                    // Default to one year back from today
+                    start_date: frappe.datetime.add_years(frappe.datetime.get_today(), -1),
                     end_date: frappe.datetime.get_today()
                 });
-                
-            }, __('View Report'), 'btn-default'); // Added to the 'View Report' group
+
+            }, __('View Report'), 'btn-primary'); // Styled as a primary button
         }
     }
 });
